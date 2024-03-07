88ffdba1ec69af5b5b549b382f51b96ae4cac40b
FearDread <ghaptonstall@gmail.com>
2024-03-07 02:18:17 -0800

    --add base v6 MUI template

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@material-ui/styles";
+import store from "./Redux/store";
+import theme from "./theme.js";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+   <React.StrictMode>
+      <Provider store={store}>
+         <ThemeProvider theme={theme}>
+            <Router>
+               <App />
+            </Router>
+         </ThemeProvider>
+      </Provider>
+   </React.StrictMode>
+);
